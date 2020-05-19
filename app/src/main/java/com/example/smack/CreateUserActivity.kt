1e278f67903ea36963b92646d6799d37368aaad5
kakoreo <ishiko951@gmail.com>
2020-05-19 15:18:15 +0900

    added create user intent

---
+++ app/src/main/java/com/example/smack/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.example.smack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun createAvatarImageViewClicked(view: View) {
+
+    }
+
+    fun createBackgroundColorBtnClicked(view: View) {
+
+    }
+
+    fun createUserBtnClucked(view: View) {
+
+    }
+}
