da20cb640f2b01b6263689cde2a48b35b4c2bc84
kakoreo <ishiko951@gmail.com>
2020-05-19 17:01:38 +0900

    added random function for picking avatar

--- app/src/main/java/com/example/smack/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/CreateUserActivity.kt
@@ -1,10 +1,17 @@
 package com.example.smack
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import kotlin.random.Random
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"  //アバターを選ばなかったときのデフォルトアイコン
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,10 +19,36 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createAvatarImageViewClicked(view: View) {
+        val random = java.util.Random()
+        val color = random.nextInt(2) //黒か白か
+        val avatar = random.nextInt(28) //アバターの数28種類
+
+        //ifを使って、アバターの色、種類を分ける
+        if (color == 0) {
+            userAvatar = "light$avatar"
+        } else {
+            userAvatar = "dark$avatar"
+        }
+
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImageView.setImageResource(resourceId)
+
 
     }
 
     fun createBackgroundColorBtnClicked(view: View) {
+        val random = java.util.Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        createAvatarImageView.setBackgroundColor(Color.rgb(r, g, b))
+
+        val savedR = r.toDouble() /255
+        val savedG = g.toDouble() /255
+        val savedB = b.toDouble() /255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
 
     }
 
